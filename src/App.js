cf6129c3548969f61352640c8a69be4167dfdf22
Alberto Valenzuela <avalen28@gmail.com>
2024-08-06 11:23:48 +0200

    F01: Adds router-dom and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import weatherService from "./services/weatherService";
+import { useEffect } from "react";
+import MainPage from "./views/MainPage";
 
 function App() {
+  // const patata = async () => {
+  //   const test = await weatherService.getWeatherDataByLongLat(
+  //     "2.1769",
+  //     "41.3825"
+  //   );
+  //   console.log(test);
+  // };
+
+  // useEffect(() => {
+  //   patata();
+  // }, []);
   return (
-    <div className="App">
-    hello!
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/views/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            hello main page
+        </div>
+    );
+};
+
+export default MainPage;
