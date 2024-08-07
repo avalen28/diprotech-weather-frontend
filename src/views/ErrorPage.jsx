e911e81bb9216de5eb03dd9cf7c926c568be65a1
Alberto Valenzuela <avalen28@gmail.com>
2024-08-07 16:39:00 +0200

    Adds error page styles

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import sadCloud from "../miscel/sadCloud.png"
+import "../styles/error.css"
 
 
 const ErrorPage = () => {
     return (
-        <div>
-            something went wrong!
-            <Link to="/">Go home!</Link>
+        <div className='error-container'>
+            <img src={sadCloud} className="error-img" alt="Error" />
+            <h2>¡Ups! algo ha salido mal...</h2>
+            <Link to="/" className='goBack'>Inicio</Link>
         </div>
     );
 };
