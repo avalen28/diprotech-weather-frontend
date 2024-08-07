e255a675bd8ae1027a611f41ff74db29e4cedabd
Alberto Valenzuela <avalen28@gmail.com>
2024-08-07 17:34:06 +0200

    Adds styles to main page and horizontal scroll for forecast

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -7,12 +7,12 @@ const WeatherCard = ({ daylyInfo }) => {
   return (
     <>
       <h2 className="forecast-day">{formatDate(daylyInfo.dt)}</h2>
+      <FontAwesomeIcon icon={printWeatherIcon(daylyInfo.weather[0].main)} />
       <div className="forecast-block-info-1">
         <div>
           <p>{toCelsius(daylyInfo.temp.max)}°C</p>
           <p>{toCelsius(daylyInfo.temp.min)}°C</p>
         </div>
-        <FontAwesomeIcon icon={printWeatherIcon(daylyInfo.weather[0].main)} />
       </div>
     </>
   );
